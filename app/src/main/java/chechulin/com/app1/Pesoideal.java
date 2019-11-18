b761e630f07453ee7accc330ec5e07353a3db4b3
Chechu <sergio.abad.ferreria@gmail.com>
2019-11-18 15:02:21 +0100

    terminada

--- app/src/main/java/chechulin/com/app1/Pesoideal.java
+++ app/src/main/java/chechulin/com/app1/Pesoideal.java
@@ -11,7 +11,6 @@ public class Pesoideal extends Activity {
     private double minbul = 16;
     private double min = 18.5;
     private double max = 24.9;
-    private double maxob = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +19,7 @@ public class Pesoideal extends Activity {
     }
 
     public void CalcularPesoIdeal(View vista){
+
         EditText edad = (EditText) findViewById(R.id.editText);
         EditText altura = (EditText) findViewById(R.id.editText2);
         TextView result = (TextView) findViewById(R.id.textView9);
@@ -27,11 +27,11 @@ public class Pesoideal extends Activity {
         double peso = Double.valueOf(String.valueOf(edad.getText()));
         double alt = Double.valueOf(String.valueOf(altura.getText()));
 
-        System.out.println("SU peso: " +  peso);
-        System.out.println("SU altura: " +  alt);
-
         String resultado = "Nada";
-        double constant = peso / alt * alt;
+
+        double aux = alt / 100;
+        double constant = peso / Math.pow(aux, 2);
+
         if (constant < max && constant > min){
             if (constant < 26.5 && constant > 25.5){
                 resultado = "Estás divin@ ;)";
@@ -44,6 +44,8 @@ public class Pesoideal extends Activity {
                     resultado = "Demasiado delgado ";
                 }if (constant >= minbul && constant <= 18.5){
                     resultado = "Estás extremadamente delgado...";
+                }else{
+                    resultado = "En los huesos";
                 }
             }else if (constant >= 25){
                 if (constant >= 25 && constant < 30){
@@ -54,7 +56,6 @@ public class Pesoideal extends Activity {
                 }
             }
         }
-        System.out.println(constant);
         result.setText(resultado);
         result.setVisibility(View.VISIBLE);
     }
